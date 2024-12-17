73a38004b5dbf4fbb7e0696b4103481852d4d1ea
andre <andre@host.docker.internal>
2024-12-17 11:41:59 +0100

    Start, inklusive inloggning och meny

---
+++ Java-Project-Management/src/ngo_2024/Start.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ngo_2024;
+
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author andre
+ */
+public class Start {
+    
+    private static InfDB idb;
+    
+    public static void main (String[] args){
+        
+        try{
+            idb = new InfDB("ngo_2024", "3306", "root", "hejsan123");
+        }
+        catch(InfException e){
+            System.out.println("Databaskopplingen misslyckades");
+            JOptionPane.showMessageDialog(null, "Databaskopplingen misslyckades. " + e.getMessage());
+        }
+        
+        new Inloggning(idb).setVisible(true);
+    }
+}
