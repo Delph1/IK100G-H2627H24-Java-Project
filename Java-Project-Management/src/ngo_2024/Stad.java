e523c9e2983e3524a7416713ab72fe048a943281
Delph1 <andreas.galistel@gmail.com>
2024-12-18 15:56:18 +0100

    Uppdaterad med kommentarer. En buggfix i metoden allaStäder.

--- Java-Project-Management/src/ngo_2024/Stad.java
+++ Java-Project-Management/src/ngo_2024/Stad.java
@@ -12,6 +12,8 @@ import oru.inf.InfException;
 
 /**
  *
+ * Klass för entiteten Stad. Innehåller såväl klassmetoder som SQL-frågor. 
+ * 
  * @author Andreas Galistel
  */
 public class Stad {
@@ -32,64 +34,55 @@ public class Stad {
         }   
         catch(InfException e)
         {
-            System.out.println("Databasen har inte uppdaterats.");
-            JOptionPane.showMessageDialog(null, "Databasen har inte uppdaterats. \n" + e.getMessage()); 
+            System.out.println("Staden har inte sparats i databasen.");
+            JOptionPane.showMessageDialog(null, "Staden har inte sparats i databasen. \n" + e.getMessage()); 
         }
     }
     
+    /**
+     * 
+     * Nedan följer get-metoder
+     * 
+     */
+    
+    /**
+     * Metod för att hämta namn på Stad
+     * @return 
+     */
     public String getNamn()
     {
         return namn;
     }
     
+    /**
+     * Metod för att hämta land för en stad.
+     * @return 
+     */
     public String getLand()
     {
         return land;
     }
     
+    /**
+     * Metod för att hämta sid för en stad
+     * @return 
+     */
     public int getSid()
     {
         return sid;
     }
-
-    public void setNamn(String namn)
-    {
-        this.namn = namn;
-        try
-        {
-        String sqlfråga = "UPDATE namn WHERE sid = " + sid + " SET namn = '" + namn + "'";
-            idb.update(sqlfråga);
-        }
-        catch(InfException e)
-        {
-            System.out.println("Databasen har inte uppdaterats.");
-            JOptionPane.showMessageDialog(null, "Databasen har inte uppdaterats. \n" + e.getMessage());
-        }
-    }
     
-    public void setLand(String land)
-    {
-        this.land = land;
-        try
-        {
-            String sqlfråga = "UPDATE land WHERE sid = " + sid + " SET name = '" + land + "'";
-            idb.update(sqlfråga);
-        }
-        catch(InfException e)
-        {
-            System.out.println("Databasen har inte uppdaterats.");
-            JOptionPane.showMessageDialog(null, "Databasen har inte uppdaterats. \n" + e.getMessage());
-        }
-
-    }
-    
+    /**
+     * Metod för att hämta ut alla städer.
+     * @return 
+     */
     public ArrayList<HashMap<String, String>> getAllaStäder()
     {
         ArrayList<HashMap<String, String>> allaStäder = new ArrayList<>();
         try
         {
             String sqlfråga = "SELECT * FROM STAD";
-            idb.fetchRows(sqlfråga);
+            allaStäder = idb.fetchRows(sqlfråga);
         }
         catch(InfException e)
         {
@@ -100,7 +93,10 @@ public class Stad {
         return allaStäder;
 
     }
-    
+    /**
+     * Metod för att hämta ut en stad från databasen.
+     * @return 
+     */
     public HashMap<String, String> getEnStad()
     {
         HashMap<String, String> enStad = new HashMap<>();
@@ -117,4 +113,50 @@ public class Stad {
         }
         return enStad;
     }
+    
+    /**
+     * 
+     * Nedan följer set-metoder
+     * 
+     */
+    
+    /**
+     * Metod för att uppdatera namn på en Stad. 
+     * @param namn 
+     */
+    public void setNamn(String namn)
+    {
+        this.namn = namn;
+        try
+        {
+            String sqlfråga = "UPDATE stad WHERE sid = " + sid + " SET namn = '" + namn + "'";
+            idb.update(sqlfråga);
+        }
+        catch(InfException e)
+        {
+            System.out.println("Databasen har inte uppdaterats.");
+            JOptionPane.showMessageDialog(null, "Databasen har inte uppdaterats. \n" + e.getMessage());
+        }
+    }
+    
+    /**
+     * Metod för att uppdatera land för en Stad
+     * @param land 
+     */
+    public void setLand(String land)
+    {
+        this.land = land;
+        try
+        {
+            String sqlfråga = "UPDATE stad WHERE sid = " + sid + " SET name = '" + land + "'";
+            idb.update(sqlfråga);
+        }
+        catch(InfException e)
+        {
+            System.out.println("Databasen har inte uppdaterats.");
+            JOptionPane.showMessageDialog(null, "Databasen har inte uppdaterats. \n" + e.getMessage());
+        }
+
+    }
+    
 }
