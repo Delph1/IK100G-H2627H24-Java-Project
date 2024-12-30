ab9a0901be8e84811f0d1a27890eec523eb2a93c
SuperPC <SuperPC@DESKTOP-AP4S396>
2024-12-30 15:57:42 +0100

    fler metoder

--- Java-Project-Management/src/ngo_2024/Projekt.java
+++ Java-Project-Management/src/ngo_2024/Projekt.java
@@ -191,7 +191,19 @@ public class Projekt {
         }
         return kostnad;
     }
-    
+    public double getKostnadPerson(int aid){
+        ArrayList<HashMap<String,String>> allaProjekt = getAllaProjektFranAid(aid);
+        ArrayList<String> pidList = new ArrayList<>();
+        double summa = 0;
+        for (HashMap<String, String> rad : allaProjekt) {
+            pidList.add(rad.get("pid"));
+        }
+        for (String ettProjekt : pidList) {
+            summa += getKostnad(Integer.parseInt(ettProjekt));
+        }
+        return summa;
+    }
+            
     /**
      * 
      * @param pid
@@ -305,7 +317,7 @@ public class Projekt {
         ArrayList<HashMap<String, String>> allaProjekt = new ArrayList<>();
         try
         {
-            String sqlfråga = "SELECT * FROM projekt where pid in (select pid from ans_proj where aid =" + aid + ")";
+            String sqlfråga = "SELECT * FROM projekt where pid in (select pid from ans_proj where aid =" + aid + ");";
             allaProjekt = idb.fetchRows(sqlfråga);
         }
         catch(InfException e)
@@ -324,7 +336,7 @@ public class Projekt {
         ArrayList<HashMap<String, String>> allaProjekt = new ArrayList<>();
         try
         {
-            String sqlfråga = "SELECT * FROM projekt where projektchef =" + aid + ")";
+            String sqlfråga = "SELECT * FROM projekt where projektchef =" + aid;
             allaProjekt = idb.fetchRows(sqlfråga);
         }
         catch(InfException e)
