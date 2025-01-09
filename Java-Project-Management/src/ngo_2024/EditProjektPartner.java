6d07884949ef39acf14c70196abeafa2a43d1ccf
Delph1 <andreas.galistel@gmail.com>
2025-01-09 12:35:09 +0100

    Edit projekt partner tagit bort main klass (#122)
    * Justerat kommentar till konstruktur
    
    * Minifx 
    
    * Fixat kommentarer
    * Tagit bort main-klass

--- Java-Project-Management/src/ngo_2024/EditProjektPartner.java
+++ Java-Project-Management/src/ngo_2024/EditProjektPartner.java
@@ -27,6 +27,8 @@ public class EditProjektPartner extends javax.swing.JFrame {
 
     /**
      * konstruktor för att se bara sina egna projekt
+     * @param idb
+     * @param queryAid
      */
     public EditProjektPartner(InfDB idb, String queryAid) {
         initComponents();
@@ -505,42 +507,6 @@ public class EditProjektPartner extends javax.swing.JFrame {
     private void closeBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeBtnActionPerformed
         this.dispose();
     }//GEN-LAST:event_closeBtnActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EditProjektPartner.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EditProjektPartner.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EditProjektPartner.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EditProjektPartner.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> allaPartnersCmb;
